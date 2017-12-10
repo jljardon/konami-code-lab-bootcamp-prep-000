9bdc293a84cdf89547395d16c951ffde9c0b642f
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-10 04:06:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 const input = document.querySelector('body');
+var index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+input.addEventListener('keydown', onKeyDownHandler );
+}
 
-
-
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ console.log(e.which)
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Success! (↑↑ ↓↓ ← → ← → B A)");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
